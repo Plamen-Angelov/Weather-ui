108f5338d1c76b3f5b19aa97f0031b75d8ba4479
plamen565 <plamen565@gmail.com>
2024-08-22 10:00:13 +0300

    Remove not needed code

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,16 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import( '../views/AboutView.vue')
-  }
+ 
 ]
 
 const router = createRouter({
